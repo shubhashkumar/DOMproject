2e961ae513735faa0bfc9005b8b3103b6431f6c3
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2023-09-26 14:05:21 +0530

    using query selector

--- task1/app.js
+++ task1/app.js
@@ -1,9 +1,10 @@
-var items= document.getElementsByClassName("list-group-item");
-for(var i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight="bold";
-}
-items[2].style.backgroundColor="green";
+
+// var items= document.getElementsByClassName("list-group-item");
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight="bold";
+// }
+// items[2].style.backgroundColor="green";
 
 // var item1= document.getElementsByClassName("list-group-item-1");
 // for(var i=0;i<item1.length;i++)
@@ -11,9 +12,16 @@ items[2].style.backgroundColor="green";
 //     item1[i].style.backgroundColor="red";
 // }
 
-var item2= document.getElementsByTagName("li");
-for(var i=0;i<item2.length;i++)
-{
-    item2[i].style.backgroundColor="yellow";
-    item2[i].innerHTML="I have changed";
-}
+// var item2= document.getElementsByTagName("li");
+// for(var i=0;i<item2.length;i++)
+// {
+//     item2[i].style.backgroundColor="yellow";
+//     item2[i].innerHTML="I have changed";
+// }
+
+//query selector
+var item2= document.querySelector(".list-group-item:nth-child(2)");
+item2.style.backgroundColor="green";
+
+var item3= document.querySelector(".list-group-item:nth-child(3)");
+item3.style.visibility = "hidden";
