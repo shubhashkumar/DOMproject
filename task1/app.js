23fc7d604301572258d2e054d859eefb36b42129
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2023-09-26 12:49:40 +0530

    second time updated

--- task1/app.js
+++ task1/app.js
@@ -4,3 +4,9 @@ for(var i=0;i<items.length;i++)
     items[i].style.fontWeight="bold";
 }
 items[2].style.backgroundColor="green";
+
+var item1= document.getElementsByClassName("list-group-item-1");
+for(var i=0;i<item1.length;i++)
+{
+    item1[i].style.backgroundColor="red";
+}
