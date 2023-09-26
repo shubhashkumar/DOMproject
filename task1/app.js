891b2af8a8c50e5fd964e0cd87fe879e8ebe11b4
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2023-09-26 13:01:12 +0530

    updated 3rd time

--- task1/app.js
+++ task1/app.js
@@ -5,8 +5,15 @@ for(var i=0;i<items.length;i++)
 }
 items[2].style.backgroundColor="green";
 
-var item1= document.getElementsByClassName("list-group-item-1");
-for(var i=0;i<item1.length;i++)
+// var item1= document.getElementsByClassName("list-group-item-1");
+// for(var i=0;i<item1.length;i++)
+// {
+//     item1[i].style.backgroundColor="red";
+// }
+
+var item2= document.getElementsByTagName("li");
+for(var i=0;i<item2.length;i++)
 {
-    item1[i].style.backgroundColor="red";
+    item2[i].style.backgroundColor="yellow";
+    item2[i].innerHTML="I have changed";
 }
